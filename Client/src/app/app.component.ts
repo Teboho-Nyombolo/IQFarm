f33f4b79e3c1d9fe2be4e1d5553e0807b7f6c4b4
radebeneo <radebeneo17@gmail.com>
2026-07-04 10:38:33 +0200

    dashboard.

--- Client/src/app/app.component.ts
+++ Client/src/app/app.component.ts
@@ -1,10 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MobileOnlyComponent } from './shared/mobile-only/mobile-only.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MobileOnlyComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
