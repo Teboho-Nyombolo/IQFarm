f4057a46bda6dbdb5682804bc67b7b99d40e4f17
Banele Luthuli <banelendlovu34@gmail.com>
2026-07-04 12:02:50 +0200

    FEAT: Seasonal Insight implementation

--- Server/src/main/java/com/example/server/controller/DashboardController.java
+++ Server/src/main/java/com/example/server/controller/DashboardController.java
@@ -1,6 +1,7 @@
 package com.example.server.controller;
 
 import com.example.server.dto.response.ApiResponse;
+import com.example.server.dto.response.SeasonalResponseDTO;
 import com.example.server.dto.response.WeatherResponseDTO;
 import com.example.server.service.DashBoardService;
 import org.springframework.http.ResponseEntity;
@@ -19,9 +20,15 @@ public class DashboardController {
         this.dashBoardService = dashBoardService;
     }
 
-    @GetMapping("/{ownerId}")
+    @GetMapping("/weather/{ownerId}")
     public ResponseEntity<ApiResponse<WeatherResponseDTO>> getDailyWeather(@PathVariable Long ownerId) {
         WeatherResponseDTO weatherResponseDTO = this.dashBoardService.getCurrentWeather(ownerId);
         return ResponseEntity.ok(ApiResponse.success(weatherResponseDTO));
+    }
+
+    @GetMapping("/seasonal/{ownerId}")
+    public ResponseEntity<ApiResponse<SeasonalResponseDTO>> getSeasonal(@PathVariable Long ownerId) {
+        SeasonalResponseDTO seasonalResponseDTO = this.dashBoardService.getCurrentSeasonal(ownerId);
+        return ResponseEntity.ok(ApiResponse.success(seasonalResponseDTO));
     }
 }
