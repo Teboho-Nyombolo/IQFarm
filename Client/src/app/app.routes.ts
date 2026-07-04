ce22980b5c905cdd5dfbf420ae59e5605e2202bc
Otshepahetse <otshepahetse.t@gmail.com>
2026-07-04 02:42:43 -0700

    added the landing page component

--- Client/src/app/app.routes.ts
+++ Client/src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { LandingPageComponent } from './features/landing-page/landing-page.component';
+import { DashboardComponent } from './features/dashboard/dashboard.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        component: LandingPageComponent
+    },
+    {
+        path: 'dashboard',
+        component: DashboardComponent
+    }
+];
